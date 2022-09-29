e81623f629e1a9e9885e29318f45549ee48143be
Oyemike Chukuneku <oyemikehc@gmail.com>
2022-09-28 17:17:24 -0700

    Task commit

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,4 +5,4 @@ def best_score(a_dictionary):
     max_score = max(a_dictionary.values(), default = None)
     for key, value in a_dictionary.items():
         if value is max_score:
-        return key
+            return key
